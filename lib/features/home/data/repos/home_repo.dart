79421024ad2d188189c2d08226172312df5a0130
diaa <diaaabuseada@gmail.com>
2025-09-12 21:48:20 +0300

    create home repo methods and failure

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookstore/core/errors/failures.dart';
+import 'package:bookstore/features/home/data/models/bookmodel/bookmodel.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  Future<void> fetchbestsellerBooks();
-  Future<void> fetchfeaturedBooks();
+  Future<Either<Failures, List<Bookmodel>>> fetchbestsellerBooks();
+  Future<Either<Failures, List<Bookmodel>>> fetchfeaturedBooks();
 }
